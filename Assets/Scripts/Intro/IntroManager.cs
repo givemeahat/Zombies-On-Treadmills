3afaab908ca3c59155e4468ea95f333e21288269
givemeahat <refrigerator60@gmail.com>
2022-07-12 17:48:04 -0400

    intro improvements

--- Assets/Scripts/Intro/IntroManager.cs
+++ Assets/Scripts/Intro/IntroManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using System.Linq;
 using TMPro;
 
 public class IntroManager : MonoBehaviour
@@ -10,16 +10,35 @@ public class IntroManager : MonoBehaviour
     public TextMeshProUGUI tutorialText;
     TextMeshProEffect tutorialTextEffect;
     public int lineTracker = 0;
+    bool hasFinishedLine = true;
+
+    public GameObject continueSymbol;
 
     public void Start()
     {
         tutorialText.text = lines[lineTracker];
         tutorialTextEffect = tutorialText.gameObject.GetComponent<TextMeshProEffect>();
         tutorialTextEffect.Play();
+        continueSymbol.GetComponent<Animator>().Play("Return Symbol_Active");
     }
-    private void Update()
+    public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)) ProgressIntro();
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!hasFinishedLine)
+            {
+                tutorialText.text = lines[lineTracker];
+                tutorialTextEffect.Finish();
+                hasFinishedLine = true;
+                return;
+            }
+            else
+            {
+                hasFinishedLine = false;
+                continueSymbol.GetComponent<Animator>().Play("Return Symbol_Inactive");
+                ProgressIntro();
+            }
+        }
     }
     public void ProgressIntro()
     {
@@ -31,5 +50,14 @@ public class IntroManager : MonoBehaviour
         }
         tutorialText.text = lines[lineTracker];
         tutorialTextEffect.Play();
+        StartCoroutine(WaitToPlayActiveAnimation());
+    }
+
+    IEnumerator WaitToPlayActiveAnimation()
+    {
+        float tutorialTimer = lines[lineTracker].Length / 50 + .5f;
+        yield return new WaitForSeconds(tutorialTimer);
+        continueSymbol.GetComponent<Animator>().Play("Return Symbol_Active");
+        hasFinishedLine = true;
     }
 }
