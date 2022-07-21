59dda88d2d1e0121f6c5ec0b608d877f2749a791
givemeahat <refrigerator60@gmail.com>
2022-07-21 17:51:38 -0400

    tweaking tutorial

--- Assets/Scripts/Intro & Tutorial/TutorialManager.cs
+++ Assets/Scripts/Intro & Tutorial/TutorialManager.cs
@@ -10,13 +10,7 @@ public class TutorialManager : MonoBehaviour
     public GameObject currentCard;
     GameObject prevCard;
 
-    float moveAmt;
-
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.D)) MoveForwards();
-        if (Input.GetKeyUp(KeyCode.A)) MoveBackwards();
-    }
+    float moveAmt = 300;
 
     public void MoveForwards()
     {
@@ -28,8 +22,8 @@ public class TutorialManager : MonoBehaviour
         prevCard = currentCard;
         tracker++;
         currentCard = cards[tracker];
-        moveAmt = 350;
-        StartCoroutine(SwitchCards());
+        Vector3 newCardLocation = new Vector3(cardGroup.transform.localPosition.x - moveAmt, 0, 0);
+        StartCoroutine(SwitchCards(newCardLocation));
     }
     
     public void MoveBackwards()
@@ -44,32 +38,33 @@ public class TutorialManager : MonoBehaviour
         prevCard = currentCard;
         tracker--;
         currentCard = cards[tracker];
-        moveAmt = -350;
-        StartCoroutine(SwitchCards());
+        Vector3 newCardLocation = new Vector3(cardGroup.transform.localPosition.x + moveAmt, 0, 0);
+        StartCoroutine(SwitchCards(newCardLocation));
     }
 
-    IEnumerator SwitchCards()
+    IEnumerator SwitchCards(Vector3 newCardLocation)
     {
         float elapsedTime = 0;
-        float waitTime = .5f;
-        Vector3 newCardLocation = new Vector3(cardGroup.transform.localPosition.x +- moveAmt, 0, 0);
+        float waitTime = .2f;
+        Vector3 startPos = cardGroup.transform.localPosition;
+        Vector3 startCurrentCardScale = currentCard.transform.localScale;
+        Vector3 startPrevCardScale = prevCard.transform.localScale;
         Vector3 newCurrentCardScale = new Vector3(1, 1, 1);
         Vector3 newPrevCardScale = new Vector3(.75f, .75f, .75f);
 
         while (elapsedTime < waitTime)
         {
-            currentCard.transform.localScale = Vector3.Slerp(currentCard.transform.localScale, newCurrentCardScale, (elapsedTime / waitTime));
-
-            prevCard.transform.localScale = Vector3.Slerp(prevCard.transform.localScale, newPrevCardScale, (elapsedTime / waitTime));
-
-            cardGroup.transform.localPosition = Vector3.Slerp(cardGroup.transform.localPosition, newCardLocation, (elapsedTime / waitTime));
+            currentCard.transform.localScale = Vector3.Lerp(startCurrentCardScale, newCurrentCardScale, (elapsedTime / waitTime));
+            prevCard.transform.localScale = Vector3.Lerp(startPrevCardScale, newPrevCardScale, (elapsedTime / waitTime));
+            cardGroup.transform.localPosition = Vector3.Lerp(startPos, newCardLocation, (elapsedTime / waitTime));
 
             elapsedTime += Time.deltaTime;
 
             yield return null;
         }
+        
+        cardGroup.transform.localPosition = newCardLocation;
         currentCard.transform.localScale = newCurrentCardScale;
         prevCard.transform.localScale = newPrevCardScale;
-        cardGroup.transform.localPosition = newCardLocation;
     }
 }
