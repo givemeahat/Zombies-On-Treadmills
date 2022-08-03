8179ed23ab635aee1d04184c189d632677c1212c
givemeahat <refrigerator60@gmail.com>
2022-08-03 14:07:18 -0400

    added potential lava shader, created animation offset for volcanoes

--- Assets/Scripts/GameBoard/Volcano.cs
+++ Assets/Scripts/GameBoard/Volcano.cs
@@ -6,11 +6,13 @@ public class Volcano : MonoBehaviour
 {
     //public GameManager gm;
     public Transform waypoint;
-
+    public GameObject smokeFX;
     // Start is called before the first frame update
     void Start()
     {
         //gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
+        smokeFX.GetComponent<Animator>().StopPlayback();
+        smokeFX.GetComponent<Animator>().Play("Smoke", 0, Random.value);
     }
 
     // Update is called once per frame
