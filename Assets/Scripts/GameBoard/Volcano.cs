ba133cce71ebce98ce1392dc1515e80d3fb955f0
givemeahat <refrigerator60@gmail.com>
2022-08-16 11:22:20 -0400

    volcano smoke transparency!

--- Assets/Scripts/GameBoard/Volcano.cs
+++ Assets/Scripts/GameBoard/Volcano.cs
@@ -7,12 +7,18 @@ public class Volcano : MonoBehaviour
     //public GameManager gm;
     public Transform waypoint;
     public GameObject smokeFX;
+    public Material smokeMat;
+
     // Start is called before the first frame update
     void Start()
     {
         //gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameManager>();
         smokeFX.GetComponent<Animator>().StopPlayback();
         smokeFX.GetComponent<Animator>().Play("Smoke", 0, Random.value);
+
+        Renderer rend = smokeFX.GetComponent<Renderer>();
+
+        rend.material = new Material(smokeMat);
     }
 
     // Update is called once per frame
