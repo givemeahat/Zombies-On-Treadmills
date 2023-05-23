1eaa17446b0b30c9b46289d946817ecccabc9948
givemeahat <refrigerator60@gmail.com>
2023-05-23 13:12:41 -0400

    generic store prefabs

--- Assets/Scripts/GameBoard/House.cs
+++ Assets/Scripts/GameBoard/House.cs
@@ -6,6 +6,19 @@ public class House : MonoBehaviour
 {
     //public GameManager gm;
     public Transform waypoint;
+    public SpriteRenderer rend;
+
+    public Sprite[] storeSprites;
+    public bool isRandom = false;
+
+    private void Awake()
+    {
+        if (isRandom)
+        {
+            int _spriteNum = Random.Range(0, storeSprites.Length);
+            rend.sprite = storeSprites[_spriteNum];
+        }
+    }
 
     // Start is called before the first frame update
     public void Flash()
